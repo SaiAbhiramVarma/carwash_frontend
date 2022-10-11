37aa76198b92f4845ff5992b8c997e0e02262ac8
SaiAbhiramVarma <102342862+SaiAbhiramVarma@users.noreply.github.com>
2022-10-11 16:26:58 +0530

    Add files via upload

---
+++ src/component/ContactComponent.jsx
@@ -0,0 +1,39 @@
+
+import React from "react";
+
+
+
+function ContactComponent() {
+  return (
+    <div className="container" align="center">
+      <h1 className="text-center">You can get us here!</h1>
+
+      <div className="contact-details">
+        <div className="row_container">
+          <div className="row">
+            <div className="column">
+              
+                  <h2>Call Us</h2>
+                  <h5>+91 9550736975</h5>
+                
+            </div>
+            <div className="column">
+              
+                  <h2>Our Office</h2>
+                  <h5>Andhra pradhesh</h5>
+                
+            </div>
+            <div className="column">
+              
+                  <h2>Send Us Mail</h2>
+                  <h5>nandini.sivakumari@gmail.com</h5>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactComponent;
